621d37450d3d049a250dfae8f2dee3ec4ab20c55
wannabemrrobot <dv.official.10010@gmail.com>
2021-09-10 00:54:59 +0530

    coursework - circular queue implementation

--- courseworks/practical-data-structures-algorithms-in-java/eclipse-workspace/becoming-leet/src/ds/queue/Queue.java
+++ courseworks/practical-data-structures-algorithms-in-java/eclipse-workspace/becoming-leet/src/ds/queue/Queue.java
@@ -17,24 +17,61 @@ public class Queue {
 	}
 	
 	public void insert(long element) {
+		// implement maxSize check to add items, to prevent ArrayOutOfBound
+		
+		// Or implement Circular Queue
+		if(rear == maxQueueSize-1) {
+			rear = -1;
+			front = 0;
+		}
+		
 		rear++;
+		System.out.println("rear incremented: " + rear);
 		this.queueArray[rear] = element;
+		System.out.println("element added to list: " + element);
 		totalItems++;
+		System.out.println("Total number of items incremented: " + totalItems);
 	}
 	
-	public void remove() {
+	public long remove() {
+		// Queue always removes the first element inserted
 		
+		long firstElement = this.queueArray[front];
+		front++;
+		
+		// front == maxQueueSize, when its the last element going to be removed
+		// thus, resetting the Queue to insert from first(Circular Queue)
+		if(front == maxQueueSize) {
+			front = 0;
+		}
+		
+		totalItems--;
+		return firstElement;
+	}
+	
+	public long peekFront() {
+		return this.queueArray[front];
+	}
+	
+	public boolean isEmpty() {
+		return (totalItems == 0);
+	}
+	
+	public boolean isFull() {
+		return (totalItems == maxQueueSize)
 	}
 	
 	public void viewQueue() {
 		System.out.print("[ ");
+		
+//		for(int i=front; i<=rear; i++) {
+//			System.out.print(this.queueArray[i] + " ");
+//		}
 		for(int i=0; i<this.queueArray.length; i++) {
 			System.out.print(this.queueArray[i] + " ");
 		}
-		System.out.print("]");
-	}
-	
-	public long peak() {
 		
+		System.out.print("]");
+		System.out.println("");
 	}
 }
--- courseworks/practical-data-structures-algorithms-in-java/eclipse-workspace/becoming-leet/src/ds/queue/driver.java
+++ courseworks/practical-data-structures-algorithms-in-java/eclipse-workspace/becoming-leet/src/ds/queue/driver.java
@@ -4,12 +4,25 @@ public class driver {
 	public static void main(String[] args) {
 		Queue queue = new Queue(5);
 		
+		System.out.println("Adding elements to the list: 10, 20, 30, 40, 50");
 		queue.insert(10);
 		queue.insert(20);
 		queue.insert(30);
 		queue.insert(40);
 		queue.insert(50);
 		
+		System.out.println("Viewing the queue after insertion: ");
+		queue.viewQueue();
+		
+		System.out.println("Removing elements from the queue: ");
+		System.out.println(queue.remove());
+		System.out.println(queue.remove());
+		System.out.println("Viewing the queue after removing: ");
+		queue.viewQueue();
+		
+		System.out.println("Adding more elements after Queue is filled: 40, 50");
+		queue.insert(40);
+		queue.insert(50);
 		queue.viewQueue();
 	}
 }
